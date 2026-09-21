9a44cc7edaa403d0e04b1746107792ba00eb39cb
Deepak Kumar Gupta <deepak030217@gmail.com>
2026-09-21 21:31:20 +0530

    initial ignire

---
+++ GitCiCDPipeLine/Controllers/CICDpipelineController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitCiCDPipeLine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CICDpipelineController : ControllerBase
+    {
+        [HttpGet("GetNewDetails")]
+        public IActionResult GetData()
+        {
+            return Ok("Hello from GetNewDetails");
+        }
+    }
+}
